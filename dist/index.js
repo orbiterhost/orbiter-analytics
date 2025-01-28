11cc7dcb61ef00da3fe699ede2f15dd159d00038
Justin Hunter <justin@orbiter.host>
2025-01-28 00:02:41 +0000

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+const app = new Hono();
+app.get('/health', (c) => {
+    return c.text('Hello from orbit!');
+});
+const port = 5000;
+console.log(`Server is running on http://localhost:${port}`);
+serve({
+    fetch: app.fetch,
+    port
+});
