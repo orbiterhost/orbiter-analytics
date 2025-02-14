72c0f9193ffe2a78ffe313a740cf7ae24e0154b8
Justin Hunter <justin@pinata.cloud>
2025-02-13 19:05:29 -0600

    added group id for db backups

--- src/pinata.ts
+++ src/pinata.ts
@@ -12,7 +12,7 @@ export const backupDatabase = async () => {
     try {
         const blob = new Blob([fs.readFileSync("./traffic.db")]);
         const file = new File([blob], `orbiter-analytics-db-${new Date()}`, { type: "text/plain"})
-        const upload = await pinata.upload.file(file);
+        const upload = await pinata.upload.file(file).group("019501f1-c849-74df-aa3e-d92218097fef");
         console.log(upload);
     } catch (error) {
         console.log("DB backup failed");
